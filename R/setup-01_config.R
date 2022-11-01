46d3fa26087dfeb90f13f1ad7722634eea790809
Alexander-Morin <alexm.14@outlook.com>
2022-11-01 16:01:04 -0700

    init script for config pathing

---
+++ R/setup-01_config.R
@@ -0,0 +1,6 @@
+## This script establishes pathing of data and plots used throughout the project
+## -----------------------------------------------------------------------------
+
+
+# Hard-coded data paths: big data on Pavlab
+
