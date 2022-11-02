729dcfb2061e4a8513149e41cafd59f22ee28d29
Alexander-Morin <alexm.14@outlook.com>
2022-11-02 14:33:27 -0700

    start on perturbmat

--- R/setup-01_config.R
+++ R/setup-01_config.R
@@ -6,8 +6,13 @@ date <- "Apr2022"  # Data freeze to use for analysis
 cores <- 8  # For use in parallel
 
 
-# Hard-coded data paths: big data on Pavlab
+# Hard-coded data paths on Pavlab
 # ------------------------------------------------------------------------------
+
+
+# Expression platform info from Nathaniel Lim
+platform_path <- "/space/grp/nlim/CronGemmaDump/AD_Dump.TSV"
+
 
 
 
@@ -47,5 +52,11 @@ meta_dir <- "/home/amorin/Data/Metadata/"
 # Where the result set files live
 rs_dir <- "/home/amorin/Data/Expression_files/Gemma/Resultsets/"
 
-# Where to save the processed objects
+# Where to save the list of processed perturbation resultsets
 expr_dir <- "/home/amorin/Data/Expression_files/Gemma/"
+
+# Where to save the perturb effect size matrices
+pmat_dir <- "/home/amorin/Data/Expression_files/Perturb_matrix/"
+
+# Top level of perturb files
+pplot_dir <- "/home/amorin/Plots/TF_perturb/"
