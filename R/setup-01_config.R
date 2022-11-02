abd35b31c46dc5b6bf64ad66d947d95a726d5401
Alexander-Morin <alexm.14@outlook.com>
2022-11-02 11:11:38 -0700

    gemma pathing added

--- R/setup-01_config.R
+++ R/setup-01_config.R
@@ -1,6 +1,51 @@
 ## This script establishes pathing of data and plots used throughout the project
-## -----------------------------------------------------------------------------
+
+
+
+date <- "Apr2022"  # Data freeze to use for analysis
+cores <- 8  # For use in parallel
 
 
 # Hard-coded data paths: big data on Pavlab
+# ------------------------------------------------------------------------------
 
+
+
+# Googlesheets IDs
+# ------------------------------------------------------------------------------
+
+
+# Perturbation metadata
+gsheets_perturb <- "1oXo1jfoPYcX94bq2F6Bqm1Es1glc8g9mnJvYAO37Vw4"
+
+
+# ChIP-seq metadata
+# gsheets_chip
+
+
+# Metadata and other genomic tables
+# ------------------------------------------------------------------------------
+
+
+meta_dir <- "/home/amorin/Data/Metadata/"
+
+
+
+
+
+# ChIP-seq 
+# ------------------------------------------------------------------------------
+
+
+
+
+
+# Perturbation
+# ------------------------------------------------------------------------------
+
+
+# Where the result set files live
+rs_dir <- "/home/amorin/Data/Expression_files/Gemma/Resultsets/"
+
+# Where to save the processed objects
+expr_dir <- "/home/amorin/Data/Expression_files/Gemma/"
