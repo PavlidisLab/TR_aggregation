0b30b4bc1ad1b02c57655714d3aeaaaa9487f04f
Alexander-Morin <alexm.14@outlook.com>
2022-11-07 15:17:26 -0800

    config pathing; load to function; minor format

--- R/chipgr-01_save_grlist.R
+++ R/chipgr-01_save_grlist.R
@@ -1,24 +1,22 @@
-## Save GRlist objects for the Batch 1 ChIP-seq experiments
+## Save ChIP-seq experiments as list of GenomicRange objects
 ## -----------------------------------------------------------------------------
 
 library(tidyverse)
 library(GenomicRanges)
-source("~/regnetR/R/utils/range_table_functions.R")
-
-date <- "Apr2022"
-peakset <- "idr"  # idr for TF (but overlap still for mecp2) or all overlap
-min_peaks <- 100
-pipeline_dir <- "/cosmos/data/pipeline-output/chipseq-encode-pipeline/chip"
-out_dir <- "~/Data/Annotated_objects/GRanges/"
+source("R/setup-01_config.R")
+source("R/utils/range_table_functions.R")
 
 # batch 1 ChIP-seq meta and directories of peak files
-run_ids <- read.delim(paste0("~/Data/Metadata/Chipseq/batch1_run_dirs_", date, ".tsv"), stringsAsFactors = FALSE)
-meta <- read.delim(paste0("~/Data/Metadata/Chipseq/batch1_chip_meta_final_", date, ".tsv"), stringsAsFactors = FALSE)
+run_ids <- read.delim(paste0(meta_dir, "Chipseq/batch1_run_dirs_", date, ".tsv"), stringsAsFactors = FALSE)
+meta <- read.delim(paste0(meta_dir, "Chipseq/batch1_chip_meta_final_", date, ".tsv"), stringsAsFactors = FALSE)
 stopifnot(all(meta$Experiment_ID %in% run_ids$Experiment_ID))
 
-# ENCODE blacklists
-bl_hg <- read.delim("~/Data/Chromosome_info/blacklist_hg38.tsv", stringsAsFactors = FALSE)
-bl_mm <- read.delim("~/Data/Chromosome_info/blacklist_mm10.tsv", stringsAsFactors = FALSE)
+# Load ENCODE blacklists and convert to GRanges
+bl_hg <- read.delim(bl_path_hg, stringsAsFactors = FALSE)
+bl_gr_hg <- bl_to_gr(bl_hg)
+
+bl_mm <- read.delim(bl_path_mm, stringsAsFactors = FALSE)
+bl_gr_mm <- bl_to_gr(bl_mm)
 
 ids <- left_join(meta, run_ids[, c("Experiment_ID", "Dir")], by = "Experiment_ID")
 
@@ -32,67 +30,46 @@ ids_mm <- ids[ids$Species == "Mouse", ]
 #-------------------------------------------------------------------------------
 
 
+load_gr_list <- function(id_df, bl_gr, peakset = "idr") {
+  
+  stopifnot(peakset %in% c("idr", "overlap"))
+  
+  gr_list <- lapply(1:nrow(id_df), function(i) {
+    
+    # Mecp2 ran on histone pipeline which produces overlap, not idr, peak sets
+    pipeline_type <- 
+      ifelse(str_detect(id_df$Experiment_ID[i], "HISTONE"), "overlap", peakset)
+    
+    # load peak table and coerce consistent chr names with protein coding table
+    peak_table <- read_encpeak(id_df$Dir[i], peakset = pipeline_type) %>% 
+      get_standard_chr()  
+    
+    # Convert to a GRanges object and remove blacklist regions.
+    # Format = TRUE will strip start and stop and only return summit
+    peak_gr <- peak_to_gr(peak_table, format = FALSE) %>% 
+      filter_blacklist(bl_gr = bl_gr)
+    
+    message(id_df$Experiment_ID[i], " complete ", Sys.time())
+    
+    return(peak_gr)
+    
+  })
+  
+  names(gr_list) <- id_df$Experiment_ID
+  
+  # Convert to GRList object
+  gr_list <- GRangesList(gr_list)
+  
+  return(gr_list)
+  
+}
+
+
 # Human
-
-
-bl_gr_hg <- bl_to_gr(bl_hg)
-
-
-gr_list_hg <- lapply(1:nrow(ids_hg), function(i) {
-  
-  pipeline_type <- 
-    ifelse(str_detect(ids_hg$Experiment_ID[i], "HISTONE"), "overlap", peakset)
-  
-  peak_table <- read_encpeak(ids_hg$Dir[i], peakset = pipeline_type) %>% 
-    get_standard_chr()  # consistent chr names with ensembl table
-  
-  # Convert to a GRanges object and remove blacklist regions.
-  # Format = TRUE will strip start and stop and only return summit
-  peak_gr <- peak_to_gr(peak_table, format = FALSE) %>% 
-    filter_blacklist(bl_gr = bl_gr_hg)
-
-  message(ids_hg$Experiment_ID[i], " complete ", Sys.time())
-  
-  return(peak_gr)
-  
-})
-  
-names(gr_list_hg) <- ids_hg$Experiment_ID
-
-# Convert to GRList object
-gr_list_hg <- GRangesList(gr_list_hg)
-
+gr_hg <- load_gr_list(id_df = ids_hg, bl_gr = bl_gr_hg)
 
 # Mouse
-
-
-bl_gr_mm <- bl_to_gr(bl_mm)
-
-
-gr_list_mm <- lapply(1:nrow(ids_mm), function(i) {
-  
-  pipeline_type <- 
-    ifelse(str_detect(ids_mm$Experiment_ID[i], "HISTONE"), "overlap", peakset)
-  
-  peak_table <- read_encpeak(ids_mm$Dir[i], peakset = pipeline_type) %>% 
-    get_standard_chr()  # consistent chr names with ensembl table
-  
-  # Convert to a GRanges object and remove blacklist regions.
-  # Format = TRUE will strip start and stop and only return summit
-  peak_gr <- peak_to_gr(peak_table, format = FALSE) %>% 
-    filter_blacklist(bl_gr = bl_gr_mm)
-  
-  message(ids_mm$Experiment_ID[i], " complete ", Sys.time())
-  
-  return(peak_gr)
-  
-})
-
-names(gr_list_mm) <- ids_mm$Experiment_ID
-
-
-# Convert to GRList object
-gr_list_mm <- GRangesList(gr_list_mm)
+gr_mm <- load_gr_list(id_df = ids_mm, bl_gr = bl_gr_mm)
 
 
 # Save 
@@ -100,12 +77,12 @@ gr_list_mm <- GRangesList(gr_list_mm)
 
 
 saveRDS(
-  object = gr_list_hg,
-  file = paste0(out_dir, "human_batch1_grlist_peakset=", peakset, "_", date, ".RDS")
+  object = gr_hg,
+  file = paste0(gr_dir, "human_batch1_grlist_", date, ".RDS")
 )
 
 
 saveRDS(
-  object = gr_list_mm,
-  file = paste0(out_dir, "mouse_batch1_grlist_peakset=", peakset, "_", date, ".RDS")
+  object = gr_mm,
+  file = paste0(gr_dir, "mouse_batch1_grlist_", date, ".RDS")
 )
