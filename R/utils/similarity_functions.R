d488c2b99b8428126d02c47796faafd3ec980bca
Alexander-Morin <alexm.14@outlook.com>
2022-11-21 15:46:38 -0800

    minor doc; rm jaccard mat

--- R/utils/similarity_functions.R
+++ R/utils/similarity_functions.R
@@ -1,9 +1,6 @@
 ## Functions to create and work with matrices and dataframes containing 
 ## similarity metrics
-## TODO: re-consider overlap matrices: parallel impl as well as whether all 4
-## matrices are required
 ## -----------------------------------------------------------------------------
-
 
 library(parallel)
 library(Matrix)
@@ -13,11 +10,11 @@ library(dplyr)
 library(WGCNA)
 
 
+# Assumes m x n mat with named rows and columns. Returns a dataframe of all
+# the row-col elements - if symmetric is TRUE, then only keep unique pairs
+# https://stackoverflow.com/questions/28035001/
+
 mat_to_df <- function(mat, symmetric = TRUE) {
-  
-  # Assumes m x n mat with named rows and columns. Returns a dataframe of all
-  # the row-col elements - if symmetric is TRUE, then only keep unique pairs
-  # https://stackoverflow.com/questions/28035001/
   
   if (symmetric) {
     df <- data.frame(
@@ -38,48 +35,22 @@ mat_to_df <- function(mat, symmetric = TRUE) {
 }
 
 
+# Remove columns that are NA in every entry
+# https://stackoverflow.com/questions/2643939/
 
 filter_na_cols <- function(mat) {
-  # Remove columns that are NA in every entry
-  # https://stackoverflow.com/questions/2643939/
+
   as.matrix(Filter(function(x) !all(is.na(x)), as.data.frame(mat)))
 }
 
 
-jaccard <- function(matrix) {
-  # Takes in a named matrix and returns the jaccard coeff for each pairwise 
-  # comparison as a matrix
-  # https://stats.stackexchange.com/a/89947
-  
-  # common values:
-  A <- Matrix::tcrossprod(matrix)
-  # indexes for non-zero common values
-  im <-  which(A > 0, arr.ind=TRUE)
-  # counts for each row
-  b <-  rowSums(matrix)
-  # only non-zero values of common
-  Aim <-  A[im]
-  # Jacard formula: #common / (#i + #j - #common)
-  jmat <-  as.matrix(Matrix::sparseMatrix(
-    i = im[,1],
-    j = im[,2],
-    x = Aim / (b[im[,1]] + b[im[,2]] - Aim),
-    dims = dim(A)
-  ))
-  
-  rownames(jmat) <- colnames(jmat) <- rownames(matrix)
-  
-  return(jmat)
-}
-
-
+# Create a data frame of elements in a named mat and add columns for the 
+# compared TFs/species and whether they are the same. If perturb is
+# TRUE, will also add a column for whether the perturbations are the same.
+# Symmetric matrix only unique elements are kept.
+# Assumes mat has rows and cols named as: [GSE]_[TF]_[Species]_[Context|Perturbation]
 
 format_pair_df <- function(mat, symmetric = TRUE, perturb = FALSE) {
-  
-  # create data frame of unique comparisons in a named mat and then add columns
-  # for the compared TFs/species and whether they are the same. If perturb is
-  # TRUE, will also add a column for whether the perurbations match
-  # Assumes mat has rows and cols named as: [GSE]_[TF]_[Species]_[Context|Perturbation]
   
   df <- mat_to_df(mat, symmetric = symmetric)
   
@@ -100,11 +71,11 @@ format_pair_df <- function(mat, symmetric = TRUE, perturb = FALSE) {
   
   group <- vapply(1:nrow(row_split), function(i) {
     if (tf1[i] == tf2[i] & species1[i] == species2[i]) {
-      return ("In_TR_in_species")
+      return("In_TR_in_species")
     } else if (tf1[i] == tf2[i] & species1[i] != species2[i]) {
-      return ("In_TR_out_species")
+      return("In_TR_out_species")
     } else {
-      return ("Out")
+      return("Out")
     }
   }, FUN.VALUE = character(1))
   
@@ -121,33 +92,65 @@ format_pair_df <- function(mat, symmetric = TRUE, perturb = FALSE) {
   if (perturb) pair_df$Same_perturb <- context1 == context2
   
   return(pair_df)
-  
 }
 
 
+# Split a pair into a list for the intra-TR experiments for each TR
+
+split_pair_df <- function(pair_df) {
+  
+  tfs <- unique(c(as.character(pair_df$TF1), as.character(pair_df$TF2)))
+  
+  tf_list <- lapply(tfs, function(x) {
+    filter(pair_df, as.character(TF1) == x & as.character(TF2) == x)
+  })
+  names(tf_list) <- tfs
+  
+  return(tf_list)
+}
+
+
+# Helper to extract top n genes from a named vector of values
+
 get_topn_genes <- function(vec, n, decrease = TRUE) {
-  # Helper to extract top n genes from a named vector of values
+  
   stopifnot(is.numeric(vec) | length(names(vec)) == 0)
   names(head(sort(vec, decreasing = decrease), n))
 }
 
 
+# Assumes vec1 and vec2 are named - return intersect names
+
 get_common <- function(vec1, vec2) {
-  # Assumes vec1 and vec2 are named - return intersect names
-  common <- intersect(names(vec1[!is.na(vec1)]), names(vec2[!is.na(vec2)]))
+  
+  intersect(names(vec1[!is.na(vec1)]), names(vec2[!is.na(vec2)]))
 }
 
+
+# Helper that takes rows of a paired df and uses mat to find out how many genes
+# were measured in common between the two experiments
+
+count_common <- function(df, mat, ncores) {
+  
+  l <- mclapply(1:nrow(df), function(x) {
+    exp1 <- df$Row[x]
+    exp2 <- df$Col[x]
+    length(intersect(names(na.omit(mat[, exp1])), names(na.omit(mat[, exp2]))))
+  }, mc.cores = ncores)
+  
+  return(unlist(l))
+}
+
+
+# Performs gene overlap for every pairwise column of a matrix, keeping only
+# the top n gene symbols for each experiment. Used on ChIP-seq on Perturbation
+# effect sizes matrices. Fills out 4 matrices of overlap metrics: jaccard 
+# coefficients, FET pvals, odds ratio, and intersect length.
 
 get_overlap_matrices <- function(mat,  # gene x exp matrix
                                  topn,  # how many top symbols to keep
                                  decrease_arg = TRUE, # decrease sort?
                                  common_only = FALSE) { # only consider shared symbols
-  
-  
-  # performs gene overlap for every pairwise column of a matrix, keeping only
-  # the top n gene symbols for each experiment. fills out 4 matrices of these
-  # comparisons: jaccard coefficients, FET pvals, odds ratio, and the length
-  # of the intersect
   
   # init output matrices
   jac_mat <- matrix(NA, nrow = ncol(mat), ncol = ncol(mat))
@@ -191,130 +194,18 @@ get_overlap_matrices <- function(mat,  # gene x exp matrix
 }
 
 
-chip_sim_list <- function(mat, topn) {
-  
-  # Mat as a gene x exp matrix - returns a list of 5 matrices of similarity
-  # metrics between the cols/exps of mat: Pcor, length of intersect of topn, 
-  # Jaccard of topn, FET pval of topn, and log odds ratio of topn
-  
-  overlap <- get_overlap_matrices(mat, topn)
-  pcor <- WGCNA::cor(mat)
-  sim_list <- append(list(Pcor = pcor), overlap)
-  return(sim_list)
-}
-
-
-perturb_sim_list <- function(fc_mat, 
-                             pval_mat, 
-                             topn, 
-                             common_arg = TRUE) {
-  
-  # mat_list must contain gene x exp pval and FC perturb matrices - generates 
-  # list of matrices containing exp by exp similarity metrics: Pcor +/- abs FC, 
-  # and for each of topn pval/upreg/downreg: length of intersect, Jaccard, FET 
-  # pval, and log odds ratio of topn
-  
-  pcor <- WGCNA::cor(fc_mat, use = "pairwise.complete.obs")
-  
-  pcor_abs <- WGCNA::cor(abs(fc_mat), use = "pairwise.complete.obs")
-  
-  # Pval - sort min to max
-  overlap_pval <- get_overlap_matrices(
-    mat = pval_mat, 
-    topn = topn, 
-    decrease_arg = FALSE,
-    common_only = common_arg)
-  
-  # Upreg - sort max to min
-  overlap_up <- get_overlap_matrices(
-    mat = fc_mat, 
-    topn = topn, 
-    decrease_arg = TRUE,
-    common_only = common_arg)
-  
-  # Downreg - sort min to max
-  overlap_down <- get_overlap_matrices(
-    mat = fc_mat, 
-    topn = topn, 
-    decrease_arg = FALSE,
-    common_only = common_arg)
- 
-  names(overlap_pval) <- paste("Pval", names(overlap_pval), sep = "_")
-  names(overlap_up) <- paste("Upreg", names(overlap_up), sep = "_")
-  names(overlap_down) <- paste("Downreg", names(overlap_down), sep = "_")
-  
-  sim_list <- c(list(Pcor = pcor), 
-                list(Pcor_abs = pcor_abs), 
-                overlap_pval, 
-                overlap_up, 
-                overlap_down)
-  return(sim_list)
-}
-
-
-count_common <- function(df, mat, ncores = 8) {
-  # Helper that takes rows of a paired df and uses mat to find out
-  # how many genes were measured in common between the correlated experiments
-  
-  unlist(mclapply(1:nrow(df), function(x) {
-    exp1 <- df$Row[x]
-    exp2 <- df$Col[x]
-    length(intersect(names(na.omit(mat[, exp1])), names(na.omit(mat[, exp2]))))
-  }, mc.cores = ncores))
-}
-
-
-format_and_merge <- function(mat_list, 
-                             gene_mat = NULL, 
-                             add_common = FALSE,
-                             symmetric_arg = TRUE) {
-  
-  # Take in a list of named similarity matrices, format each into a df of the
-  # unique pairs, rename generic stat column to the current stat, then merge all
-  # into a single df. If add_common is TRUE, add column of how many genes are
-  # commonly measured between the paired experiments. Symmetric refers to the
-  # original gene x experiment matrices used to generate similarity between 
-  # exps/cols: perturb/chip produce symmetric matrices (so only want unique
-  # elements) while intersect produces non-symmetric (want everything)
-  
-  stats <- names(mat_list)
-  stats <- stats[!str_detect(stats, "^.*_Genes")]  # Hacky removal of intersect list of genes (not sim mat)
-  
-  
-  sim_list <- lapply(stats, function(x) {
-    
-    df <- format_pair_df(mat_list[[x]], symmetric = symmetric_arg)
-    
-    df <- mutate(df, Key = paste(Row, Col, sep = ":")) %>% 
-      relocate(Value, .after = last_col())
-    
-    colnames(df)[colnames(df) == "Value"] <- x
-    
-    return(df)
-  })
-  
-  join_names <- Reduce(intersect, lapply(sim_list, colnames))
-  df <- plyr::join_all(sim_list, by = join_names) %>% select(-Key)
-  
-  if (add_common) {
-    df$Common_genes <- count_common(df, gene_mat)
-  }
-
-  return(df)
-}
-
+# Performs gene overlap for every pairwise column of a matrix, keeping only
+# the top n gene symbols for each experiment. Used *BETWEEN* columns of ChIP-seq
+# and Perturbation effect sizes matrices. Fills out 4 matrices of overlap 
+# metrics: jaccard coefficients, FET pvals, odds ratio, intersect length. Along
+# with these matrices, also export a character vector of the overlapping genes
+# between each comparison.
 
 overlap_chip_perturb <- function(chip_mat,  # gene x exp matrix,
                                  perturb_mat,  # gene x exp matrix
                                  topn,  # how many top symbols to keep
                                  decrease_perturb = TRUE, # decrease sort for perturb?
                                  common_only = FALSE) { # only consider shared symbols
-  
-  
-  # performs gene overlap for every pairwise columns/experiments of chip binding
-  # matrix and a peturb effect size matrix, keeping only the top n gene symbols 
-  # for each experiment. fills out 4 matrices of these comparisons: jaccard, 
-  # FET pvals, odds ratio, and the length of the intersect
   
   # init output matrices
   jac_mat <- matrix(NA, nrow = ncol(chip_mat), ncol = ncol(perturb_mat))
@@ -373,14 +264,77 @@ overlap_chip_perturb <- function(chip_mat,  # gene x exp matrix,
 }
 
 
+# Mat as a gene x exp matrix - returns a list of 5 matrices of similarity
+# metrics between the cols/exps of mat: Pcor, length of intersect of topn, 
+# Jaccard of topn, FET pval of topn, and log odds ratio of topn
+
+chip_sim_list <- function(mat, topn) {
+
+  overlap <- get_overlap_matrices(mat, topn)
+  pcor <- WGCNA::cor(mat)
+  sim_list <- append(list(Pcor = pcor), overlap)
+  
+  return(sim_list)
+}
+
+
+# Generates list of similarity matrices between columns of perturbation mat.
+# Pearson cor of raw and absolute fold changes. And for each of sorting by
+# pvals, positive FCs and negative FCs: length of intersect of topn, Jaccard of 
+# topn, FET pval of topn, and log odds ratio of topn
+
+perturb_sim_list <- function(fc_mat, 
+                             pval_mat, 
+                             topn, 
+                             common_arg = TRUE) {
+  
+  pcor <- WGCNA::cor(fc_mat, use = "pairwise.complete.obs")
+  
+  pcor_abs <- WGCNA::cor(abs(fc_mat), use = "pairwise.complete.obs")
+  
+  # Pval - sort min to max
+  overlap_pval <- get_overlap_matrices(
+    mat = pval_mat, 
+    topn = topn, 
+    decrease_arg = FALSE,
+    common_only = common_arg)
+  
+  # Upreg - sort max to min
+  overlap_up <- get_overlap_matrices(
+    mat = fc_mat, 
+    topn = topn, 
+    decrease_arg = TRUE,
+    common_only = common_arg)
+  
+  # Downreg - sort min to max
+  overlap_down <- get_overlap_matrices(
+    mat = fc_mat, 
+    topn = topn, 
+    decrease_arg = FALSE,
+    common_only = common_arg)
+ 
+  names(overlap_pval) <- paste("Pval", names(overlap_pval), sep = "_")
+  names(overlap_up) <- paste("Upreg", names(overlap_up), sep = "_")
+  names(overlap_down) <- paste("Downreg", names(overlap_down), sep = "_")
+  
+  sim_list <- c(list(Pcor = pcor), 
+                list(Pcor_abs = pcor_abs), 
+                overlap_pval, 
+                overlap_up, 
+                overlap_down)
+  
+  return(sim_list)
+}
+
+
+# Perform overlap_chip_perturb() for each of sorting perturbation data by
+# pvals, positive FC, and negative FC
+
 intersect_sim_list <- function(chip_mat, 
                                fc_mat, 
                                pval_mat, 
                                topn, 
                                common_arg = TRUE) {
-  
-  # Perform overlap_chip_perturb() for each of sorting perturbation data by
-  # pvals, positive FC, and negative FC
   
   # Pval - sort min to max
   overlap_pval <- overlap_chip_perturb(
@@ -411,17 +365,59 @@ intersect_sim_list <- function(chip_mat,
   names(overlap_down) <- paste("Downreg", names(overlap_down), sep = "_")
   
   sim_list <- c(overlap_pval, overlap_up, overlap_down)
+  
   return(sim_list)
 }
 
 
-get_summary <- function(df) {
+# Takes in a list of named similarity matrices, formats each into a df of the
+# unique pairs, rename generic stat column to the current stat, then merge all
+# into a single df. If add_common is TRUE, add column of how many genes are
+# commonly measured between the paired experiments. Symmetric refers to the
+# original gene x experiment matrices used to generate similarity between 
+# exps/cols: perturb/chip produce symmetric matrices (so only want unique
+# elements) while intersect produces non-symmetric (want everything)
+
+format_and_merge <- function(mat_list, 
+                             gene_mat = NULL, 
+                             add_common = FALSE,
+                             symmetric_arg = TRUE) {
   
-  # Return a dataframe of the min/max/mean/median of numeric cols by Group
+  stats <- names(mat_list)
+  
+  # Hacky removal of intersect object's list of genes (which is not sim mat)
+  stats <- stats[!str_detect(stats, "^.*_Genes")]
+  
+  # List of formatted dfs for each similarity statistic 
+  sim_list <- lapply(stats, function(x) {
+    
+    df <- format_pair_df(mat_list[[x]], symmetric = symmetric_arg) %>%
+      mutate(df, Key = paste(Row, Col, sep = ":")) %>% 
+      relocate(Value, .after = last_col()) %>% 
+      dplyr::rename({{x}} := Value)
+    
+    return(df)
+  })
+  
+  # Get all stat names and then join all dfs into one
+  join_names <- Reduce(intersect, lapply(sim_list, colnames))
+  
+  df <- plyr::join_all(sim_list, by = join_names) %>% select(-Key)
+  
+  if (add_common) {
+    df$Common_genes <- count_common(df, gene_mat, ncores)
+  }
+
+  return(df)
+}
+
+
+# Return a dataframe of the min/max/mean/median of numeric cols by Group
+
+get_summary <- function(df) {
   
   df %>% 
     group_by(Group) %>% 
-    select_if(is.numeric) %>%
     dplyr::summarise(across(where(is.numeric), list(
       Min = ~min(.x, na.rm = TRUE),
       Max = ~max(.x, na.rm = TRUE),
@@ -431,15 +427,19 @@ get_summary <- function(df) {
 }
 
 
+# get_summary for every unique TF in df
+
 tf_summary <- function(df) {
   
-  # get_summary for every unique TF in df
-  
   tfs <- unique(c(as.character(df$TF1), as.character(df$TF2)))
+  
   tf_list <- lapply(tfs, function(x) {
     tf_df <- filter(df, as.character(TF1) == x | as.character(TF2) == x)
     get_summary(tf_df)
   })
   names(tf_list) <- tfs
+  
   return(tf_list)
 }
+
+
