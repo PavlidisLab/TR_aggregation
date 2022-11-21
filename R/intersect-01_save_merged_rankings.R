195fcf2b48f36ba3a053fce1105a9ba60a41e8ba
Alexander-Morin <alexm.14@outlook.com>
2022-11-20 21:09:48 -0800

    config pathing; improve doc

--- R/intersect-01_save_merged_rankings.R
+++ R/intersect-01_save_merged_rankings.R
@@ -1,45 +1,41 @@
-## Loads and organizes the ChIP-seq and perturbation data matrices, and create
-## a single data frame for each TR/species of the summarized/aggregated target
-## rankings for export
+## Loads and organizes the ChIP-seq and perturbation data matrices into a single
+## list for export, and creates a single data frame for each TR/species of the 
+# summarized/aggregated target rankings for export.
 ## -----------------------------------------------------------------------------
 
 library(plyr)
 library(tidyverse)
 library(data.table)  # frank() for rank using two variables
+source("R/setup-01_config.R")
 
-date <- "Apr2022"  # most recent data freeze
-rank_outfile <- paste0("~/scratch/R_objects/", date, "_ranked_target_list.RDS")
-dat_outfile <- paste0("~/scratch/R_objects/", date, "_all_data_list.RDS")
+# rank for the final ranking dfs by TR 
+# dat collects all the contributing summarized data into one list
+rank_outfile <- paste0(scratch_dir, date, "_ranked_target_list.RDS")
+dat_outfile <- paste0(scratch_dir, date, "_all_data_list.RDS")
 
 # Loading perturb data
-fdr <- 0.1
-perturb_meta <- read.delim(paste0("~/Data/Metadata/Gemma/batch1_tfperturb_meta_final_", date, ".tsv"), stringsAsFactors = FALSE)
-perturb_hg <- readRDS(paste0("~/Data/Expression_files/Perturb_matrix/human_list_perturb_matrix_", date, ".RDS"))
-perturb_mm <- readRDS(paste0("~/Data/Expression_files/Perturb_matrix/mouse_list_perturb_matrix_", date, ".RDS"))
-perturb_ortho <- readRDS(paste0("~/Data/Expression_files/Perturb_matrix/ortho_list_perturb_matrix_", date, ".RDS"))
-tf_de <- readRDS(paste0("~/Data/TF_perturb/TF_FDR=0.1_counts_list_", date, ".RDS"))
+perturb_meta <- read.delim(paste0(meta_dir, "Gemma/batch1_tfperturb_meta_final_", date, ".tsv"), stringsAsFactors = FALSE)
+perturb_hg <- readRDS(paste0(pmat_dir, "human_list_perturb_matrix_", date, ".RDS"))
+perturb_mm <- readRDS(paste0(pmat_dir, "mouse_list_perturb_matrix_", date, ".RDS"))
+perturb_ortho <- readRDS(paste0(pmat_dir, "ortho_list_perturb_matrix_", date, ".RDS"))
+tf_de <- readRDS(paste0(expr_dir, "TF_perturb_DE_counts_list_by_TF_FDR01_", date, ".RDS"))
 
 # Loading ChIP-seq data
-intensity_flag <- FALSE  # use ouyang scores with or without macs2 intensity
-binary_dist <- 25e3  # distance threshold used for binary gene scores
-min_peaks <- 100  # how many peaks were required to keep an experiment
-peakset <- "idr"  # idr for TF (but overlap still for mecp2) or all overlap
-chip_dir <- "~/Data/Annotated_objects/Bind_matrices/Encpipe/" # bind matrix path
 chip_type <- "QN_log"  # which chip processing scheme to use
-chip_meta <- read.delim(paste0("~/Data/Metadata/Chipseq/batch1_chip_meta_final_", date, ".tsv"), stringsAsFactors = FALSE)
-chip_hg <- readRDS(paste0(chip_dir, "Human_refseq_", date, "_processed_bindmat_list_minpeak=", min_peaks, "_ouyang_dc=5000_intensity=", intensity_flag, "_binary=", binary_dist/1e3, "kb_peakset=", peakset, ".RDS"))
-chip_mm <- readRDS(paste0(chip_dir, "Mouse_refseq_", date, "_processed_bindmat_list_minpeak=", min_peaks, "_ouyang_dc=5000_intensity=", intensity_flag, "_binary=", binary_dist/1e3, "kb_peakset=", peakset, ".RDS"))
-chip_ortho <- readRDS(paste0(chip_dir, "Ortho_refseq_", date, "_processed_bindmat_list_minpeak=", min_peaks, "_ouyang_dc=5000_intensity=", intensity_flag, "_binary=", binary_dist/1e3, "kb_peakset=", peakset, ".RDS"))
-bind_summary <- readRDS(paste0("~/scratch/R_objects/", date, "_refseq_bind_summary_peakset=", peakset, ".RDS"))
+chip_meta <- read.delim(paste0(meta_dir, "Chipseq/batch1_chip_meta_final_", date, ".tsv"), stringsAsFactors = FALSE)
+chip_hg <- readRDS(paste0(cmat_dir, "Human_refseq_", date, "_processed_bindmat_list_minpeak=", min_peaks, "_ouyang_dc=5000_intensity=FALSE_binary=", binary_dist/1e3, "kb.RDS"))
+chip_mm <- readRDS(paste0(cmat_dir, "Mouse_refseq_", date, "_processed_bindmat_list_minpeak=", min_peaks, "_ouyang_dc=5000_intensity=FALSE_binary=", binary_dist/1e3, "kb.RDS"))
+chip_ortho <- readRDS(paste0(cmat_dir, "Ortho_refseq_", date, "_processed_bindmat_list_minpeak=", min_peaks, "_ouyang_dc=5000_intensity=FALSE_binary=", binary_dist/1e3, "kb.RDS"))
+bind_summary <- readRDS(paste0(scratch_dir, date, "_refseq_bind_summary.RDS"))
 
 # low throughput resource of curated targets
-lt_curated <- read.delim("~/Data/Metadata/Curated_targets_all_July2022.tsv", stringsAsFactors = FALSE)
+lt_curated <- read.delim(paste0(meta_dir, "Curated_targets_all_July2022.tsv"), stringsAsFactors = FALSE)
 
 # Map of orthologous genes 
-pc_ortho <- read.delim("~/Data/Metadata/hg_mm_1to1_ortho_genes_DIOPT-v8.tsv", stringsAsFactors = FALSE)
+pc_ortho <- read.delim(paste0(meta_dir, "hg_mm_1to1_ortho_genes_DIOPT-v8.tsv"), stringsAsFactors = FALSE)
 
 
-# Organize and export data matrices
+# Organize all effect size matrices and metadata tables into a single list
 # ------------------------------------------------------------------------------
 
 
@@ -66,8 +62,9 @@ perturb_list <- list(
 )
 
 
-# For binding include raw binding scores, quantile norm of log (used for all
-# analysis), and the binary assignment
+# For binding include raw binding scores, quantile norm of log10(mat+1) (used 
+# for most analysis), and the binary assignment
+
 
 keep_norm <- c("Raw", "QN_log", "Binary")
 
@@ -81,17 +78,21 @@ bind_list <- list(
 
 all_dat <- list(Binding = bind_list, Perturbation = perturb_list)
 
-saveRDS(all_dat, file = dat_outfile)
 
-
-# Join each aggregated TR table such that each species/TR combo has a single df 
-# of evidence, and add rankings
+# Join summarized TR tables from ChIP-seq and perturbation experiments so that 
+# each TR/species has a single dataframe of evidence per gene.
+# ChIP-seq: mean binding scores, proportion binary, binding specificity model output 
+# Perturbation experiments: DE counts, Average absolute FC, purity, DE prior
+# Then, add column for whether the given TR-gene pair has curated evidence.
+# Finally, add integer ranking of the aggregated ChIP-seq evidence (mean binding
+# score), aggregated perturbation (count DE, ties broken by average abs FC), and
+# an integrated ranking that takes the rank product between these two lists.
 # ------------------------------------------------------------------------------
 
 
+# Formats the limma summary table in preparation for joining
+
 prepare_bind_fit <- function(bind_fit) {
-  
-  # Formats the limma summary table in preparation for joining
   
   bind_fit <- bind_fit %>% 
     rownames_to_column(var = "Symbol") %>% 
@@ -102,14 +103,13 @@ prepare_bind_fit <- function(bind_fit) {
 }
 
 
+# Extracts the curated targets for the given TF, specifying species to handle
+# case matching or the orthologous gene name structure [Human_symbol]_[Mouse_symbol]
 
 get_targets <- function(curated_df, 
                         tf, 
                         species,  # Human|Mouse|Ortho
                         ortho_genes = pc_ortho) {
-  
-  # Extracts the curated targets for the given TF, specifying species to handle
-  # case matching or the orthologous gene name structure [Human_symbol]_[Mouse_symbol]
   
   if (species == "Human") {
     targets <- curated_df %>%
@@ -139,34 +139,41 @@ get_targets <- function(curated_df,
 }
 
 
+# Append the ranks of each gene for the perturbation evidence (count DE) and
+# the ChIP-seq binding signal (mean bind score). If FC_truebreak is TRUE,
+# then DE count ties are broken by the average absolute FC of the gene. Then
+# add the rank product of these two lines of evidence, as in the BETA algo.
+# Because "top ranked" genes from the RP have arbitrary small units, take
+# the rank of the rank product to yield a final integer rank where rank=1
+# has the strongest evidence.
+# BETA: https://pubmed.ncbi.nlm.nih.gov/24263090/
+# R application: https://pubmed.ncbi.nlm.nih.gov/32894066/
 
 add_ranks <- function(df, FC_tiebreak = TRUE) {
   
-  # Append the ranks of each gene for the perturbation evidence (count DE) and
-  # the ChIP-seq binding signal (mean bind score). If FC_truebreak is TRUE,
-  # then DE count ties are broken by the average absolute FC of the gene. Then
-  # add the rank product of these two lines of evidence, as in the BETA algo.
-  # Because "top ranked" genes from the RP have arbitrary small units, take
-  # the rank of the rank product to yield a final integer rank where rank=1
-  # has the strongest evidence.
-  # BETA: https://pubmed.ncbi.nlm.nih.gov/24263090/
-  # R application: https://pubmed.ncbi.nlm.nih.gov/32894066/
-  
   if (FC_tiebreak) {
+    
     df$Rank_perturbation <- data.table::frank(
       list(-df$Count_DE, -df$Avg_abs_FC), ties.method = "min")
+  
   } else {
-    df$Rank_perturbation <- rank(-df$Count_DE, ties = "min")
+    
+    df$Rank_perturbation <- rank(-df$Count_DE, ties.method = "min")
+  
   }
   
   df$Rank_binding <- rank(-df$Mean_bind)
   rank_prod <- df$Rank_binding/nrow(df) * df$Rank_perturbation/nrow(df)
-  df$Rank_integrated <- rank(rank_prod, ties = "min")
+  df$Rank_integrated <- rank(rank_prod, ties.method = "min")
   
   return(arrange(df, Rank_integrated))
 }
 
 
+
+# For a single TF, merge the perturbation table, the bind fit table, and the
+# bind summary table, then append presence of curated targets, and finally
+# add the ranks of the lines of evidence
 
 merge_tf <- function(de_table,
                      bind_fit = NULL, 
@@ -175,17 +182,17 @@ merge_tf <- function(de_table,
                      species,
                      tf) {
   
-  # For a single TF, merge the perturbation table, the bind fit table, and the
-  # bind summary table, then append presence of curated targets, and finally
-  # add the ranks of the lines of evidence
-  
   targets <- get_targets(curated_df = curated_df, species = species, tf = tf)
   
-  if (!is.null(bind_fit)) {
+  if (!is.null(bind_fit)) {  # ortho doesn't have bind fit
+    
     bind_fit <- prepare_bind_fit(bind_fit)
     merge_df <- plyr::join_all(list(de_table, bind_summary, bind_fit), by = "Symbol")
+  
   } else {
+    
     merge_df <- plyr::join_all(list(de_table, bind_summary), by = "Symbol")
+  
   }
   
   merge_df$Curated_target <- merge_df$Symbol %in% targets
@@ -196,14 +203,13 @@ merge_tf <- function(de_table,
 
 
 
+# Applies merge_tf over list of tables
+
 merge_all <- function(de_list,
                       bind_fit_list = NULL,
                       bind_summary_list,
                       curated_df = lt_curated,
                       species) {
-  
-  # Applies merge_tf over list of tables
-  
   
   tfs <- intersect(names(de_list), names(bind_summary_list))
   
@@ -217,12 +223,13 @@ merge_all <- function(de_list,
              tf = x)
   })
   names(tf_list) <- tfs
+  
   return(tf_list)
 }
 
 
 
-all_list <- list(
+rank_list <- list(
   
   Human = merge_all(de_list = tf_de$Human,
                     bind_fit_list = bind_summary$Human_block_fit,
@@ -242,4 +249,5 @@ all_list <- list(
 
 # Save out
 
-saveRDS(all_list, rank_outfile)
+saveRDS(all_dat, file = dat_outfile)
+saveRDS(rank_list, rank_outfile)
